086c1cc2faad2ea8b715f7f47cac7831c5fb1064
Alex  Slone <alexjslone12@gmail.com>
2024-06-08 13:44:47 -0700

    updating with connection to cloud console

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "remote" {
+    organization = "alex27_Org"
+    workspaces {
+      name = "webServer-ModuleTest"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,7 +15,7 @@ terraform {
 
 module "ec2_instance" {
   source  = "app.terraform.io/alex27_Org/ec2-instanceDemo1/aws"
-  version = "1.0.0"  # Replace with the correct version tag you pushed
+  version = "1.0.0"  # Ensure this matches the tag in your GitHub repo
 }
 
 provider "aws" {
